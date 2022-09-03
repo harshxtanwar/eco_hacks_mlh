bfa55b2f316dbc42b417df41c8c58e122aff2fcf
Laleet Borse <laleet@Laleets-MacBook-Air.local>
2022-09-03 21:34:16 +0530

    host_done_linked

--- eco_hacks/application/urls.py
+++ eco_hacks/application/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('home/browse_event/', views.browseEvents),
     path('home/browse_event/<id>', views.register),
     path('home/browse_event/registration_done/<id>', views.regDone),
-    path('hosting_done/', views.hostDone)
+    path('home/post_event/hosting_done/', views.hostDone)
     
 ]
--- eco_hacks/application/views.py
+++ eco_hacks/application/views.py
@@ -8,56 +8,59 @@ from con_func.function import query_by_id, post_db, get_db_browse
 from con_func.emails import email_send
 
 
-
-
-
 def homepage(request):
-    he = {1 : "coming from backend"}
-    ## NO backend code required here
-    return render(request, 'home.html', {'hel':he})
+    he = {1: "coming from backend"}
+    # NO backend code required here
+    return render(request, 'home.html', {'hel': he})
+
 
 def postEvent(request):
-    ## DATABASE CONNECTION MONGODB
+    return render(request, 'hosting.html')
+
+
+def hostDone(request):
+    # DATABASE CONNECTION MONGODB
     # collection = mongoConnection_collection()
     if request.method == 'POST':
         print(request.POST.get('event_name'))
         post_db({
-            'your_name':request.POST.get('your_name'),
-            'email':request.POST.get('email'),
-            'mobile_no':request.POST.get('mobile_no'),
-            'event_name':request.POST.get('event_name'),
-            'event_date':request.POST.get('event_date'),
-            'event_time':request.POST.get('event_time'),
-            'event_location':request.POST.get('event_location'),
-            'event_detail':request.POST.get('event_detail')
+            'your_name': request.POST.get('your_name'),
+            'email': request.POST.get('email'),
+            'mobile_no': request.POST.get('mobile_no'),
+            'event_name': request.POST.get('event_name'),
+            'event_date': request.POST.get('event_date'),
+            'event_time': request.POST.get('event_time'),
+            'event_location': request.POST.get('event_location'),
+            'event_detail': request.POST.get('event_detail')
         })
-    ## POST
-    return render( request, 'hosting.html')
+    # POST
+    return render(request, 'host_done.html')
+
 
 def browseEvents(request):
-    ## DATABASE CONNECTION MONGODB
+    # DATABASE CONNECTION MONGODB
     if request.method == "GET":
 
         query = get_db_browse(request)
     return render(request, 'browsing.html', {'query': query})
 
 
-def regDone(request,id):
+def regDone(request, id):
 
     if request.method == "POST":
         name = request.POST.get('your_name')
         email = request.POST.get('email')
         email_send(name, email)
         print(id)
-    ## No Backend Code Needed 
-    ## Button to redirect to the homepage
-    ## get data to register for the event
-    return render(request,'reg_done.html')
+    # No Backend Code Needed
+    # Button to redirect to the homepage
+    # get data to register for the event
+    return render(request, 'reg_done.html')
 
-def hostDone(request):
-    return render(request, 'host_done.html') 
+
+
 
 def register(request, id):
     if request.method == "GET":
         query, id = query_by_id(id)
-    return render(request, 'register.html', {'query': query, 'id':id})
+    return render(request, 'register.html', {'query': query, 'id': id})
